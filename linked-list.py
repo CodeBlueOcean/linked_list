0c4627cae5c62b0328434c349c85d2b5aaebce67
CodeBlueOcean <95150255+CodeBlueOcean@users.noreply.github.com>
2022-02-08 16:06:26 -0600

    Add files via upload

---
+++ linked-list.py
@@ -0,0 +1,17 @@
+
+# What is a Node? like a scavenger hunt where each location has treasure (Value) & directions to next location (Next)
+
+# Head Node
+# Value = "Alice"
+# Next = memory address of next node
+
+# Value = "Bob"
+# Next = memory address of 
+# next node
+
+# Value = "Eve"
+# Next = None
+# Tail Node
+
+
+
